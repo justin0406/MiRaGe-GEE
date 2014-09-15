d1c8e7d857946c7e08f92be1e6da930d9c7b6223
justin0406 <411488070@qq.com>
2014-09-15 16:22:48 +0800

    update build.sh

--- mirage-gee-build.sh
+++ mirage-gee-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CROSS_COMPILE=${HOME}/linaro_A15_201408/arm-cortex_a15-linux-gnueabihf-linaro_4.9.2-2014.08/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=${HOME}/kernel/linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.2-2014.09/bin/arm-cortex_a15-linux-gnueabihf-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=JustinXu
