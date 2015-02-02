95673809ab4fa82330b0ee7f033174712fa2c797
justin0406 <411488070@qq.com>
2015-02-03 01:15:05 +0800

    update build.sh

--- mirage-gee-build.sh
+++ mirage-gee-build.sh
@@ -1,8 +1,138 @@
 #!/bin/bash
-export CROSS_COMPILE=${HOME}/kernel/linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.2-2014.09/bin/arm-cortex_a15-linux-gnueabihf-
+
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
+KERNEL="zImage"
+DEFCONFIG="gee_defconfig"
+
+# Kernel Details
+BASE_AK_VER="MiRaGe"
+VER=".011515.gee"
+AK_VER="$BASE_AK_VER$VER"
+
+# AK Variables
+export LOCALVERSION=~`echo $AK_VER`
+export CROSS_COMPILE=${HOME}/kernel/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a15-linux-gnueabihf-
 export ARCH=arm
 export SUBARCH=arm
-export KBUILD_BUILD_USER=JustinXu
-export KBUILD_BUILD_HOST="BuildBox"
-make "gee_defconfig"
-make -j9
+export KBUILD_BUILD_USER=Justin
+export KBUILD_BUILD_HOST=BuildBox
+
+# Paths
+KERNEL_DIR=`pwd`
+REPACK_DIR="${HOME}/kernel/GEE-AnyKernel"
+PATCH_DIR="${HOME}/kernel/GEE-AnyKernel/patch"
+MODULES_DIR="${HOME}/kernel/GEE-AnyKernel/patch/modules"
+ZIP_MOVE="${HOME}/kernel/MiRaGe/MiRaGe-releases/"
+ZIMAGE_DIR="${HOME}/kernel/MiRaGe/MiRaGe-GEE/arch/arm/boot"
+
+# Functions
+function clean_all {
+		rm -rf $MODULES_DIR/*
+		cd $REPACK_DIR
+		rm -rf $KERNEL
+		git reset --hard > /dev/null 2>&1
+		git clean -f -d > /dev/null 2>&1
+		cd $KERNEL_DIR
+		echo
+		make clean && make mrproper
+}
+
+function make_kernel {
+		echo
+		make $DEFCONFIG
+		make $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR
+}
+
+function make_modules {
+		rm `echo $MODULES_DIR"/*"`
+		find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+}
+
+
+function make_zip {
+		cd $REPACK_DIR
+		zip -r9 `echo $AK_VER`.zip *
+		mv  `echo $AK_VER`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+
+echo "---------------"
+echo "Kernel Version:"
+echo "---------------"
+
+echo -e "${red}"; echo -e "${blink_red}"; echo "$AK_VER"; echo -e "${restore}";
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making Kernel:"
+echo "-----------------"
+echo -e "${restore}"
+
+while read -p "Do you want to clean stuffs (y/n)? " cchoice
+do
+case "$cchoice" in
+	y|Y )
+		clean_all
+		echo
+		echo "All Cleaned now."
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo
+
+while read -p "Do you want to build kernel (y/n)? " dchoice
+do
+case "$dchoice" in
+	y|Y)
+		make_kernel
+		make_modules
+		make_zip
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
+
